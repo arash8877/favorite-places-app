3d9a1b334513d457b9419fc465b3b4689e77a1b5
Arash <arash8877@gmail.com>
2024-05-09 11:09:20 +0200

    styled placeItem

--- components/places/PlaceItem.js
+++ components/places/PlaceItem.js
@@ -1,13 +1,16 @@
 import { View, Image, Text, Pressable, StyleSheet } from "react-native";
+import { Colors } from "../../constants/colors";
 
 const PlaceItem = ({ place, onSelect }) => {
- 
   return (
-    <Pressable onPress={onSelect}>
-      <Image source={{ uri: place.imageUri }} />
-      <View>
-        <Text>{place.title}</Text>
-        <Text>{place.address}</Text>
+    <Pressable
+      style={({ pressed }) => [styles.item, pressed && styles.pressed]}
+      onPress={onSelect}
+    >
+      <Image source={{ uri: place.imageUri }} style={styles.image} />
+      <View style={styles.info}>
+        <Text style={styles.title}>{place.title}</Text>
+        <Text style={styles.address}>{place.address}</Text>
       </View>
     </Pressable>
   );
@@ -15,4 +18,38 @@ const PlaceItem = ({ place, onSelect }) => {
 
 export default PlaceItem;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  item: {
+    flexDirection: "row",
+    alignItems: "flex-start",
+    borderRadius: 6,
+    marginVertical: 12,
+    backgroundColor: Colors.primary500,
+    elevation: 2,
+    shadowColor: "black",
+    shadowOpacity: 0.15,
+    shadowOffset: { width: 1, height: 1 },
+    shadowRadius: 2,
+  },
+  pressed: {
+    opacity: 0.7,
+  },
+  image: {
+    flex: 1,
+    borderBottomLeftRadius: 4,
+    borderTopLeftRadius: 4,
+  },
+  info: {
+    flex: 2,
+    padding: 12,
+  },
+  title: {
+    fontWeight: "black",
+    fontSize: 18,
+    color: Colors.gray700,
+  },
+  address: {
+    fontSize: 12,
+    color: Colors.gray700,
+  },
+});
--- components/places/PlacesList.js
+++ components/places/PlacesList.js
@@ -16,6 +16,7 @@ const PlacesList = ({ places }) => {
       data={places}
       keyExtractor={(item) => item.id}
       renderItem={({ item }) => <PlaceItem place={item} />}
+      style={styles.list}
     />
   );
 };
@@ -23,6 +24,9 @@ const PlacesList = ({ places }) => {
 export default PlacesList;
 
 const styles = StyleSheet.create({
+  list: {
+    margin: 24,
+  },
   fallbackContainer: {
     flex: 1,
     justifyContent: "center",
